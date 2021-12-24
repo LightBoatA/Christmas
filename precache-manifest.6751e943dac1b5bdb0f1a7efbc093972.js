9ba7594fb86ed3e61318612e2dbdde81d305e7a9
zhangsasa <2717301343@qq.com>
2021-12-24 22:56:04 +0800

    add all

---
+++ precache-manifest.6751e943dac1b5bdb0f1a7efbc093972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f10dd9f6bd4fe845634",
+    "url": "css/app.d5efef68.css"
+  },
+  {
+    "revision": "42d6957e4306b9a4241274a3299c83ab",
+    "url": "img/white-snowflake.42d6957e.png"
+  },
+  {
+    "revision": "660012c63ce848d642c48046b10e1046",
+    "url": "index.html"
+  },
+  {
+    "revision": "6f10dd9f6bd4fe845634",
+    "url": "js/app.17880c7d.js"
+  },
+  {
+    "revision": "d70d284b037266cdd0b3",
+    "url": "js/chunk-vendors.dcc8d3d5.js"
+  },
+  {
+    "revision": "cb6e59656de7f56b9dff1a98e481709d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b7257bef81af1fc70a1cb09ca85b445b",
+    "url": "media/Just The Two Of Us - Bill Withers.b7257bef.mp3"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
